b637df56efb0683f49120fdd92c5e4fb348c9400
Hyperlink Jay <158026757+hlinkJay@users.noreply.github.com>
2024-02-29 11:53:19 +0530

    [+] Add image in details screen

--- lib/beerDetailsScreen.dart
+++ lib/beerDetailsScreen.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BeerDetailsScreen extends StatelessWidget {
-
   final BeerDetails beer;
 
   const BeerDetailsScreen({super.key, required this.beer});
@@ -39,75 +38,115 @@ class BeerDetailsScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      body: Stack(
         children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 20, top: 25),
-            child: Text(beer.name!,maxLines: 2,overflow: TextOverflow.ellipsis,style: textStyle(
+          buildTopView(),
+          Align(
+              alignment: Alignment.bottomCenter,
+              child: buildBottomView(context)),
+          Positioned(
+              right: 0,
+              top: 130,
+              child: Image.network(
+                beer.imageUrl!,
+                height: 200,
+                width: 150,
+              )),
+        ],
+      ),
+    );
+  }
+
+  Column buildTopView() {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 20, top: 25),
+          child: Text(
+            beer.name!,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: textStyle(
               Colors.white,
               'Anta-Regular',
               22,
-            ),),
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 20),
-            child: ElevatedButton(onPressed: () {}, child: const Text("975 p")),
-          ),
-          const Spacer(),
-          SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: Container(
-              height: MediaQuery.sizeOf(context).height * 0.65,
-              width: MediaQuery.sizeOf(context).width,
-              decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(30),
-                      topLeft: Radius.circular(30)),
-                  color: Colors.white),
-              child: Padding(
-                padding: const EdgeInsets.all(25.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [const Text("Vol."), Text("${beer.volume!.value.toString()} ${beer.volume?.unit!}")]),
-                        const SizedBox(width: 25,),
-                        Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [const Text("Abv."), Text(beer.abv.toString())]),
-                      ],
-                    ),
-                    const SizedBox(height: 25,),
-                    const Text("Grape"),
-                    const Text("Tempranilho, 2015"),
-
-                    const SizedBox(height: 25,),
-
-                    const Text("Region"),
-                    const Text("Spain, Ribera del\nDuero"),
-
-                    const SizedBox(height: 20,),
-                    Text(beer.description!,maxLines: 10,overflow: TextOverflow.ellipsis,),
-                    const Spacer(),
-                    SizedBox(
-                      width: MediaQuery.sizeOf(context).width,
-                      height: 55,
-                      child: roundedElevatedButton('Add to cart', Colors.black, () async {
-                      },15),
-                    )
-                  ],
-                ),
-              ),
             ),
           ),
-        ],
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 20),
+          child: ElevatedButton(onPressed: () {}, child: const Text("975 p")),
+        ),
+      ],
+    );
+  }
+
+  SingleChildScrollView buildBottomView(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Container(
+        height: MediaQuery.sizeOf(context).height * 0.65,
+        width: MediaQuery.sizeOf(context).width,
+        decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(30), topLeft: Radius.circular(30)),
+            color: Colors.white),
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text("Vol."),
+                        Text(
+                            "${beer.volume!.value.toString()} ${beer.volume?.unit!}")
+                      ]),
+                  const SizedBox(
+                    width: 25,
+                  ),
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Text("Abv."),
+                        Text(beer.abv.toString())
+                      ]),
+                ],
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              const Text("Grape"),
+              const Text("Tempranilho, 2015"),
+              const SizedBox(
+                height: 25,
+              ),
+              const Text("Region"),
+              const Text("Spain, Ribera del\nDuero"),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                beer.description!,
+                maxLines: 10,
+                overflow: TextOverflow.ellipsis,
+              ),
+              const Spacer(),
+              SizedBox(
+                width: MediaQuery.sizeOf(context).width,
+                height: 55,
+                child: roundedElevatedButton(
+                    'Add to cart', Colors.black, () async {}, 15),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
